cfc7c1965853ecc04c6a09606c0e7ab019e64c47
Lucas <lucasgarciasouza12@gmail.com>
2024-09-25 17:16:57 -0300

    dff

--- aprendendoJS/curso2/index.js
+++ aprendendoJS/curso2/index.js
@@ -1,15 +1,29 @@
 class Cliente{
     nome;
     cpf;
+}
+
+class ContaCorrente{
     agencia;
     saldo;
+    sacar(valor){
+        // let valor = prompt("Digite o valor que deseja sacar:");
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
 
 cliente1.nome = "Ricardo";
 cliente1.cpf = 111222233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0.0;
+
+const contaCorrente1 = new ContaCorrente();
+contaCorrente1.saldo = 0;
+contaCorrente1.agencia = 1001;
+
+
+console.log(cliente1);
 
 
