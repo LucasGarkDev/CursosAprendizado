9bae2e1ed3a7bc38c1482cb4fc87226a8fe5f564
Lucas <lucasgarciasouza12@gmail.com>
2024-09-25 17:58:30 -0300

    kk

--- aprendendoJS/curso2/index.js
+++ aprendendoJS/curso2/index.js
@@ -5,12 +5,21 @@ class Cliente{
 
 class ContaCorrente{
     agencia;
-    saldo;
+    #saldo = 0;
     sacar(valor){
         // let valor = prompt("Digite o valor que deseja sacar:");
-        if(this.saldo >= valor){
-            this.saldo -= valor;
+        if(this.#saldo >= valor){
+            this.#saldo -= valor;
+            return valor;
         }
+        
+    }
+
+    depositar(valor){
+        if (valor <= 0) {
+            return;
+        }
+        this.#saldo += valor;
     }
 }
 
@@ -20,10 +29,12 @@ cliente1.nome = "Ricardo";
 cliente1.cpf = 111222233309;
 
 const contaCorrente1 = new ContaCorrente();
-contaCorrente1.saldo = 0;
 contaCorrente1.agencia = 1001;
 
+contaCorrente1.depositar(100);
+contaCorrente1.depositar(200);
+contaCorrente1.depositar(-1);
+const valorSacado = contaCorrente1.sacar(50);
 
-console.log(cliente1);
 
 
