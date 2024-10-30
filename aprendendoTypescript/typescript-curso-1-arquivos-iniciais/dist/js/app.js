8212a5e2f02df16186d8cae552f3bea4b6fbda23
Lucas <lucasgarciasouza12@gmail.com>
2024-10-30 16:57:54 -0300

    sdds

--- aprendendoTypescript/typescript-curso-1-arquivos-iniciais/dist/js/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const negociacao_js_1 = require("./models/negociacao.js");
-const negociacao = new negociacao_js_1.Negociacao(new Date(), 10, 100);
-console.log(negociacao.volume);
-alert("Ola Mundo!");
